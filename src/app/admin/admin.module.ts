f30af62865bed01bcb8a5b0c53ceaa544554017c
Mustapha EL KHALFAOUI <melkhalfaoui@gmail.com>
2020-05-19 17:56:01 +0200

    rajout routes

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,13 +4,28 @@ import { AdminComponent } from './admin/admin.component';
 import { AjoutAgentComponent } from './ajout-agent/ajout-agent.component';
 import { ListeAgentsComponent } from './liste-agents/liste-agents.component';
 import { ListeDemandesComponent } from './liste-demandes/liste-demandes.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const adminRoutes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      { path: 'ajout-agent', component: AjoutAgentComponent },
+      { path: 'liste-demandes', component: ListeDemandesComponent },
+      { path: 'liste-agents', component: ListeAgentsComponent },
+      { path: '',   redirectTo: 'admin', pathMatch: 'full' }
+    ],
+  }
+];
 
 
 @NgModule({
   declarations: [AdminComponent, AjoutAgentComponent, ListeAgentsComponent, ListeDemandesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(adminRoutes),
+
   ]
 })
 export class AdminModule { }
