956e3dc3dd525ba067f5b32c77f4146f3353510e
hantute <ahinfohistopro@gmail.com>
2020-05-20 17:43:28 +0200

    modifdemande

--- src/app/data/demandesData.ts
+++ src/app/data/demandesData.ts
@@ -1,62 +1,64 @@
 import { DemandeOuverture } from './../models/demande-ouverture';
 
 export const DEMANDES: DemandeOuverture[] = [
-  {
-    iddemandeOuverture: 1,
-    dateDemande: new Date('2017-08-28'),
-    valide: 1,
-    clientpotentiel: {
-        idclientpotentiel: 1,
-        nom: 'toto',
-        prenom: null,
-        email: null,
-        adresse: null,
-        telephone: null,
-        revenuMensuel: null,
-        piecesJustif: null
-    },
-    admin: {
-        idadmin: 1,
-        matricule: '001',
-        utilisateur: {
-            idutilisateur: 1,
-            nom: null,
+    {
+        iddemandeOuverture: 1,
+        dateDemande: new Date('2017-08-28'),
+        valide: 1,
+        agentMatricule: null,
+        clientpotentiel: {
+            idclientpotentiel: 1,
+            nom: 'toto',
             prenom: null,
-            email: 'admin@gmail.com',
+            email: null,
             adresse: null,
             telephone: null,
-            pseudo: null,
-            mdp: 'mdp'
+            revenuMensuel: null,
+            piecesJustif: null
+        },
+        admin: {
+            idadmin: 1,
+            matricule: '001',
+            utilisateur: {
+                idutilisateur: 1,
+                nom: null,
+                prenom: null,
+                email: 'admin@gmail.com',
+                adresse: null,
+                telephone: null,
+                pseudo: null,
+                mdp: 'mdp'
+            }
         }
-    }
-},
-{
-    iddemandeOuverture: 2,
-    dateDemande: new Date('2017-08-28'),
-    valide: 1,
-    clientpotentiel: {
-        idclientpotentiel: 2,
-        nom: 'titi',
-        prenom: null,
-        email: null,
-        adresse: null,
-        telephone: null,
-        revenuMensuel: null,
-        piecesJustif: null
     },
-    admin: {
-        idadmin: 1,
-        matricule: '001',
-        utilisateur: {
-            idutilisateur: 1,
-            nom: null,
+    {
+        iddemandeOuverture: 2,
+        dateDemande: new Date('2017-08-28'),
+        valide: 1,
+        agentMatricule: null,
+        clientpotentiel: {
+            idclientpotentiel: 2,
+            nom: 'titi',
             prenom: null,
-            email: 'admin@gmail.com',
+            email: null,
             adresse: null,
             telephone: null,
-            pseudo: null,
-            mdp: 'mdp'
+            revenuMensuel: null,
+            piecesJustif: null
+        },
+        admin: {
+            idadmin: 1,
+            matricule: '001',
+            utilisateur: {
+                idutilisateur: 1,
+                nom: null,
+                prenom: null,
+                email: 'admin@gmail.com',
+                adresse: null,
+                telephone: null,
+                pseudo: null,
+                mdp: 'mdp'
+            }
         }
     }
-}
 ];
--- src/app/models/demande-ouverture.ts
+++ src/app/models/demande-ouverture.ts
@@ -5,7 +5,9 @@ export class DemandeOuverture {
   iddemandeOuverture: number;
   dateDemande: Date;
   valide: number;
+  agentMatricule: number;
   clientpotentiel: ClientPotentiel;
   admin: Admin;
+
 }
 
