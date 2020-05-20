91884b8384f75f8a8b00421f35950a1098ae9367
hantute <ahinfohistopro@gmail.com>
2020-05-20 10:55:02 +0200

    AdminModel

---
+++ src/app/data/adminData.ts
@@ -0,0 +1,16 @@
+import { Admin } from '../models/admin';
+
+export const ADMIN: Admin = {
+    idadmin: 1,
+    matricule: '001',
+    utilisateur: {
+        idutilisateur: 1,
+        nom: null,
+        prenom: null,
+        email: 'admin@gmail.com',
+        adresse: null,
+        telephone: null,
+        pseudo: null,
+        mdp: 'mdp'
+    }
+}
